d21498535059f07a36ca958442250f6969ba3e59
Aditya Potluri <adityapotluri1@gmail.com>
2020-08-29 16:59:44 -0400

    more work done lmao idk

--- KeyValueLib.py
+++ KeyValueLib.py
@@ -12,48 +12,78 @@ class Disconnect(Exception): pass
 Error = namedtuple('Error', ('message',))
 
 
+class CommandError(Exception):pass
+class Disconnect(Exception):pass
+
+
 class ProtocolHandler(object):
-    def handle_request(self, socket_file):
-        # Parse a request from the client into it's component parts.
-        pass
+    def __init__(self):
+         handlers = {
+            '+':self.handleSimpleString,
+            '-':self.handleError,
+            ':':self.handleInteger,
+            '$':self.handleBinaryString,
+            '*':self.handleArray,
+            '%':self.handleDictionary,
+            '$':self.handleNull
+        }
+    def handleRequest(self, socket_file):
+       pass
 
-    def write_response(self, socket_file, data):
-        # Serialize the response data and send it to the client.
-        pass
+    def writeResponse(self, socket_file, data):
+       pass
 
+    def handleString(self, socket_file) ->str:
+        return socket_file.readline().rstrip('\r\n')
 
+    def handleError(self, socket_file):
+        return Error(socket_file.readline().rstrip('\r\n'))
+
+    def handleInteger(self, socket_file):
+        return int(socket_file.readline().rstrip('\r\n'))
+
+    def handleBinaryString(self, socket_file):
+        length = int(socket_file.readline().rstrip('\r\n'))
+        if length == -1:
+            return None
+        length += 2
+        return socket_file.read(length)[:-2]
+    
+    def handleArray(self, socket_file):
+        length = int(socket_file.readline().rstrip('\r\n'))
+        return [self.handleRequest(socket_file) for i in range(length)]
+    
+    def handleDict(self, socket_file):
+        length = int(socket_file.readline().rstrip('\r\n'))
+        elements = [self.handleRequest(socket_file) for i in range(length * 2)]
+        return dict(zip(elements[::2], elements[1::2]))
 class Server(object):
-    def __init__(self, host='127.0.0.1', port=31337, max_clients=64):
-        self._pool = Pool(max_clients)
+    def __init__(self, host = '127.0.0.1', port = 31337, maxClients = 64):
+        self._pool = Pool(maxClients)
         self._server = StreamServer(
-            (host, port),
-            self.connection_handler,
-            spawn=self._pool)
-
-        self._protocol = ProtocolHandler()
+            (host,port),
+            self.connectionHandler,
+            spawn = self._pool
+        )
         self._kv = {}
+    
+    def connectionHandler(self, conn:socket, handler):
+        socketFile = conn.makefile('rwb')
 
-    def connection_handler(self, conn, address):
-        # Convert "conn" (a socket object) into a file-like object.
-        socket_file = conn.makefile('rwb')
-
-        # Process client requests until client disconnects.
         while True:
             try:
-                data = self._protocol.handle_request(socket_file)
+                data = self._protocol.handleRequest(socketFile)
             except Disconnect:
                 break
 
             try:
-                resp = self.get_response(data)
+                resp = self.getResponse(data)
             except CommandError as exc:
                 resp = Error(exc.args[0])
+            
+            self._protocol.writeResponse(socketFile,resp)
 
-            self._protocol.write_response(socket_file, resp)
-
-    def get_response(self, data):
-        # Here we'll actually unpack the data sent by the client, execute the
-        # command they specified, and pass back the return value.
+    def getResponse(self, data):
         pass
 
     def run(self):
